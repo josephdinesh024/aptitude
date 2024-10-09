f094dc9df1468af7b4ca80d7d55341ef572085a5
josephdinesh024 <140788878+josephdinesh024@users.noreply.github.com>
2024-10-09 10:43:53 +0530

    Update page.js result

--- app/result/page.js
+++ app/result/page.js
@@ -8,14 +8,14 @@ const page = async() => {
     if(score)
         score=JSON.parse(score)
     // await new Promise((resolve)=>setTimeout(resolve,3000))
-    
+    const length = score?.length-1
     
   return (
     <div className="">
         <Link href='/' className="absolute top-0 btn btn-ghost text-xl">Home</Link>
         <div className="m-12 mt-28">
-            <div className="radial-progress text-primary" style={{ "--value": score[9]*10 }} role="progressbar">
-                {score[9]*10}%
+            <div className="radial-progress text-primary" style={{ "--value": score[length]*10 }} role="progressbar">
+                {score[length]*10}%
             </div>
         </div>
     </div>
